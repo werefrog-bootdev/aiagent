2a8227b707de62038aa45b648153f982a14bdfa4
Michel San <msan@hotmail.fr>
2025-06-27 17:59:37 +0200

    L4: Input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -9,11 +10,26 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 api_model = os.environ.get("GEMINI_API_MODEL", "gemini-2.0-flash-001")
 
-client = genai.Client(api_key=api_key)
+if not api_key:
+    print("Error: environment variable 'GEMINI_API_KEY' is missing.")
+    sys.exit(1)
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-response = client.models.generate_content(model=api_model,contents=prompt)
-print(response.text)
 
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+def ask_question(prompt):
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model=api_model,contents=prompt)    
+    print(response.text)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
+def main(prompt):
+    ask_question(prompt)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error: no prompt provided.\nUsage: python main.py \"Your prompt here\"")
+        sys.exit(1)
+
+    main(prompt=sys.argv[1])
